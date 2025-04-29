366510058ae3d42eac3c04ece28b2248bece5db7
Patrick <patrick.andre@student.moringaschool.com>
2025-04-29 20:52:35 +0300

    Commit

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,27 @@
 import React from "react";
 
-function PlantCard() {
+function PlantCard({ plant, onUpdatePlant }) {
+  const handleSoldOut = () => {
+    fetch(`http://localhost:6001/plants/${plant.id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ soldOut: !plant.soldOut }),
+    })
+      .then((res) => res.json())
+      .then((updatedPlant) => onUpdatePlant(updatedPlant));
+  };
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: ${plant.price.toFixed(2)}</p>
+      <button
+        className={plant.soldOut ? "" : "primary"}
+        onClick={handleSoldOut}
+      >
+        {plant.soldOut ? "Out of Stock" : "In Stock"}
+      </button>
     </li>
   );
 }
